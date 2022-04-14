09eceb35b3a8ac07f10369eaf60a3305d2305215
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2022-04-14 22:06:06 +0200

    Initial commit

---
+++ src/main/java/fun/saelatice/latice/Latice.java
@@ -0,0 +1,15 @@
+package fun.saelatice.latice;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Latice extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+    }
+}
