23f5c8c974dd708a94451c0cc8cbf2e511343cb6
Paul Rivière <paul.riviere@etu.unilim.fr>
2022-05-05 08:52:28 +0200

    Add board view

---
+++ src/main/java/fun/saelatice/latice/controller/BoardController.java
@@ -0,0 +1,19 @@
+package fun.saelatice.latice.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class BoardController {
+
+    @FXML
+    private GridPane idGdpBoard;
+
+    @FXML
+    private GridPane idGdpRack;
+    
+    @FXML
+    public void initializeBoard() {
+    	
+    }
+
+}
